b015f07a234df004e2e752a1212a98d0e9908872
Dhafin Kamil <dhafin.kamil@gmail.com>
2025-03-13 09:57:54 +0700

    “menambahkan class DataGeneric”.

--- tpmodul5_2211104018/Program.cs
+++ tpmodul5_2211104018/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> dataGeneric = new DataGeneric<string>("2211104018");
+        dataGeneric.PrintData();
+    }
+}
